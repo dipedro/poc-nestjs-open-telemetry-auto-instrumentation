a7f0f31dc1ba027101f057d383f0027a3595bf6f
Pedro Azevedo <dipedrozd@hotmail.com>
2024-05-02 11:52:31 -0300

    wip: change trace and metrics from console to vendor service

--- src/registerInstrumentation.ts
+++ src/registerInstrumentation.ts
@@ -1,5 +1,4 @@
 import {
-	ConsoleSpanExporter,
 	SimpleSpanProcessor,
 } from '@opentelemetry/sdk-trace-base';
 import { NodeSDK } from '@opentelemetry/sdk-node';
@@ -8,16 +7,28 @@ import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import * as process from 'process';
 import { Resource } from '@opentelemetry/resources';
 import { SEMRESATTRS_SERVICE_NAME, SEMRESATTRS_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 
-const traceExporter = new ConsoleSpanExporter();
+const baseLimeConfig = (route) => ({
+	url: 'vendor base URL' + route,
+	headers: {
+		'x-api-key': ''
+	},
+});
+
+const traceExporter = new OTLPTraceExporter(baseLimeConfig('traces'));
 
 export const otelSDK = new NodeSDK({
-
+	traceExporter,
+	metricReader: new PeriodicExportingMetricReader({
+		exporter: new OTLPMetricExporter(baseLimeConfig('metrics')),
+	  }),
 	resource: new Resource({
 		[SEMRESATTRS_SERVICE_NAME]: 'api-poc-auto-instrumentation',
 		[SEMRESATTRS_SERVICE_VERSION]: '1.0',
 	}),
-	traceExporter: traceExporter,
 	spanProcessor: new SimpleSpanProcessor(traceExporter),
 	instrumentations: [
 		new HttpInstrumentation(), 

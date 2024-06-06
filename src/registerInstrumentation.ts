aae969b501d72036508163b84def9042b246e2a8
Pedro Azevedo <dipedrozd@hotmail.com>
2024-06-06 11:25:17 -0300

    feat: add new default config to generate traces by auto instrumentation

--- src/registerInstrumentation.ts
+++ src/registerInstrumentation.ts
@@ -1,38 +1,52 @@
-import {
-	SimpleSpanProcessor,
-} from '@opentelemetry/sdk-trace-base';
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
-import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
-import * as process from 'process';
-import { Resource } from '@opentelemetry/resources';
-import { SEMRESATTRS_SERVICE_NAME, SEMRESATTRS_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
-import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
+import { PgInstrumentation } from '@opentelemetry/instrumentation-pg';
+import { Resource, envDetector, hostDetector, osDetector, processDetector } from '@opentelemetry/resources';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { SEMRESATTRS_SERVICE_NAME, SEMRESATTRS_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
+import * as process from 'process';
 
-const baseLimeConfig = (route) => ({
-	url: 'vendor base URL' + route,
-	headers: {
-		'x-api-key': ''
-	},
+const collectorConfig = (route) => ({
+	url: 'http://opentelemetry-collector:4318/v1/' + route
 });
 
-const traceExporter = new OTLPTraceExporter(baseLimeConfig('traces'));
+const traceExporter = new OTLPTraceExporter(collectorConfig('traces'));
+
+const pgTraceOptions = {
+	addSqlCommenterCommentToQueries: true,
+	enabled: true,
+	enhancedDatabaseReporting: true,
+	requireParentSpan: true,
+};
 
 export const otelSDK = new NodeSDK({
 	traceExporter,
-	metricReader: new PeriodicExportingMetricReader({
-		exporter: new OTLPMetricExporter(baseLimeConfig('metrics')),
-	  }),
 	resource: new Resource({
 		[SEMRESATTRS_SERVICE_NAME]: 'api-poc-auto-instrumentation',
 		[SEMRESATTRS_SERVICE_VERSION]: '1.0',
 	}),
-	spanProcessor: new SimpleSpanProcessor(traceExporter),
+	//logRecordProcessor: new OTLPLogExporter(collectorConfig('logs')),
+	/* metricReader: new PeriodicExportingMetricReader({
+		exporter: new OTLPMetricExporter(collectorConfig('metrics')),
+	}), */
+	/* spanProcessors: [new SimpleSpanProcessor(traceExporter)], */
+	/* instrumentations: [
+		getNodeAutoInstrumentations(),
+		new PgInstrumentation(),
+	], */
+	/* textMapPropagator: new CompositePropagator({
+		propagators: [new W3CBaggagePropagator(), new W3CTraceContextPropagator()],
+	  }), */
 	instrumentations: [
-		new HttpInstrumentation(), 
-		new ExpressInstrumentation(),
+		new NestInstrumentation(),
+		new PgInstrumentation(pgTraceOptions),
+		//getNodeAutoInstrumentations()
+	],
+	resourceDetectors: [
+	  envDetector,
+	  hostDetector,
+	  osDetector,
+	  processDetector,
 	],
 });
 
